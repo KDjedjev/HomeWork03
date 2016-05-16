f56f97af49efc2432fdafe88eaa98e1dda4189ff
kokodzh <kdjedjev@gmail.com>
2016-05-16 09:22:21 +0300

    Hello.

---
+++ src/task2/Main.java
@@ -0,0 +1,20 @@
+package task2;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.print("a:");
+		int a = input.nextInt();
+		System.out.print("b:");
+		int b = input.nextInt();
+		if(a>b){
+			System.out.print("A > B");
+		}else{
+		System.out.print("B > A");
+		}
+		input.close();
+	}
+}
